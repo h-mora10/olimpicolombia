2707eadb6c9bda2bfb5a9bfd1259c1c935d08011
jasmo2 <jasmo2.com@gmail.com>
2016-08-14 18:06:12 -0500

    initial setting for database postgressSQL

--- OlimpiColombia/settings.py
+++ OlimpiColombia/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'OlimpiColombia.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'OlimpiColombia',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
