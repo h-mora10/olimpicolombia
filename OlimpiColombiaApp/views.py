761924440a12f44411e561439352e0f9b1157ef5
jasmo2 <jasmo2.com@gmail.com>
2016-08-15 16:22:30 -0500

    set index controller AND pass value to template

--- OlimpiColombiaApp/views.py
+++ OlimpiColombiaApp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponseBadRequest
+
+## Models
+from OlimpiColombiaApp.models import *
 
 # Create your views here.
+
+def index(request):
+    #ToDo write a seed file
+    sports = Sport.objects.order_by(('name').asc())
+    return render(request, 'OlimpiColombiaApp/index.html',{'sports': sports})
