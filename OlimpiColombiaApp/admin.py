7221a5bb729ca3008a561654b8416d61f18da5ef
Henry Mora <hank8617@gmail.com>
2016-08-15 22:47:38 -0500

    Modulo de administración de entidades

--- OlimpiColombiaApp/admin.py
+++ OlimpiColombiaApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Athlete, Coach, Sport, Event
 # Register your models here.
+admin.site.register(Coach)
+admin.site.register(Athlete)
+admin.site.register(Sport)
+admin.site.register(Event)
