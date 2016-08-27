cb982dc2776a9bcce00a434f8e3d8af6a83bf200
dianaespc <dpaolac@gmail.com>
2016-08-26 22:32:02 -0500

    Configuraciones iniciales de OAUTH2 en facebook

---
+++ OlimpiColombiaApp/static/js/facebook.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Diana on 8/26/16.
+ */
+
+ window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '964171530358798',
+      xfbml      : true,
+      version    : 'v2.7'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
