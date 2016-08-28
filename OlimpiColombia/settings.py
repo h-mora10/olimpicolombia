06e63f3ba2ca7c40cd4dc22a5819617afab0d7dc
Henry Mora <hank8617@gmail.com>
2016-08-28 01:58:52 -0500

    Se actualiza la variable SECRET_KEY del settings, se configura como variable de entorno, su valor está en el Drive, NO OLVIDAR ACTUALIZAR LAS VARIABLES DE ENTORNO

--- OlimpiColombia/settings.py
+++ OlimpiColombia/settings.py
@@ -23,7 +23,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's-2+4#njn*tl_1v@n^tj@^4)_i#w4e5zo0_sau_01+nlrk0+q$'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
